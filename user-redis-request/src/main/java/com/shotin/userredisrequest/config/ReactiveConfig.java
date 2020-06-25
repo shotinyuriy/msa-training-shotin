8d41b2617d93003d070437c277f9fdae4d279e49
Shotin <iushotin@neoflex.ru>
2020-06-25 12:08:05 +0400

    MSA-17 used a boundedElastic scheduler for Reactive resource

---
+++ user-redis-request/src/main/java/com/shotin/userredisrequest/config/ReactiveConfig.java
@@ -0,0 +1,22 @@
+package com.shotin.userredisrequest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class ReactiveConfig {
+
+    @Bean
+    public Scheduler boundedElasticScheduler() {
+        return Schedulers.boundedElastic();
+    }
+
+    @PostConstruct
+    public void setSchedulersFactory() {
+        Schedulers.setFactory(new Schedulers.Factory() {});
+    }
+}
--- user-redis-request/src/main/java/com/shotin/userredisrequest/rest/BankAccountInfoRestResource.java
+++ user-redis-request/src/main/java/com/shotin/userredisrequest/rest/BankAccountInfoRestResource.java
@@ -4,6 +4,7 @@ import com.shotin.consumer.redis.model.BankAccountInfoEntity;
 import com.shotin.userredisrequest.rest.model.BankAccountInfoKeys;
 import com.shotin.userredisrequest.service.BankAccountInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 
 import java.util.ArrayList;
 
@@ -20,15 +22,20 @@ public class BankAccountInfoRestResource {
 
     private BankAccountInfoService bankAccountInfoService;
 
-    public BankAccountInfoRestResource(@Autowired BankAccountInfoService bankAccountInfoService) {
+    private Scheduler reactiveScheduler;
+
+    public BankAccountInfoRestResource(@Autowired BankAccountInfoService bankAccountInfoService,
+                                       @Autowired @Qualifier("boundedElasticScheduler") Scheduler reactiveScheduler) {
         this.bankAccountInfoService = bankAccountInfoService;
+        this.reactiveScheduler = reactiveScheduler;
     }
 
     @GetMapping("/keys")
     public Mono<BankAccountInfoKeys> bankAccountInfoKeys() {
         return bankAccountInfoService.findAllKeys()
                 .collect(ArrayList<String>::new, ArrayList::add)
-                .map(BankAccountInfoKeys::new);
+                .map(BankAccountInfoKeys::new)
+                .subscribeOn(reactiveScheduler);
     }
 
     @GetMapping(value = "/{uuid}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -36,6 +43,7 @@ public class BankAccountInfoRestResource {
         return bankAccountInfoService
                 .findById(uuid)
                 .flatMap(bankAccountInfoEntity -> Mono.just(ResponseEntity.ok(bankAccountInfoEntity)))
-                .onErrorReturn(ResponseEntity.<BankAccountInfoEntity>notFound().build());
+                .onErrorReturn(ResponseEntity.<BankAccountInfoEntity>notFound().build())
+                .subscribeOn(reactiveScheduler);
     }
 }
